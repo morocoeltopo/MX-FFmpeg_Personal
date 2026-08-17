ab43df505d4378e982f18d1d28e48cd3095156bd
iDNA420 <80810457+iDNA420@users.noreply.github.com>
2026-08-17 18:16:40 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ BUILD_ROOT="${MX_FF_SRC_DIR}/jni"
 
 VERSION=${VERSION:="2.7.0"}
 BUILD_NUMBER="0"
-MX_FF_SRC_URL="https://amazon-source-code-downloads.s3.us-east-1.amazonaws.com/MXPlayer/client/mxplayer-2.7.0-ffmpeg-v4.2-src.tar.gz"
+MX_FF_SRC_URL="https://amazon-source-code-downloads.s3.us-east-1.amazonaws.com/MXPlayer/client/mxplayer-3.1.0-ffmpeg-v4.2-src.tar.gz"
 SRC_FILENAME="ffmpeg-src.tar.gz"
 
 die() {
